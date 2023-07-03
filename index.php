9a9fcffb465b6cdaf939e68e39fec2907145ab1e
Sekoubambs <amadou.haidara@gmail.com>
2023-07-03 14:35:56 +0200

    Maj

--- index.php
+++ index.php
@@ -12,9 +12,10 @@ session_start()
     <title>Document</title>
     <link rel="stylesheet" href="style.css">
 </head>
+
 <body>
 
-
+<div class=container>
 
 <header class="header">
 
@@ -27,12 +28,19 @@ session_start()
 
     <nav>
 
-        <ul>
+        <ul class=d-inline-block>
 
-            <a class="navbar" href='index.php'>Accueil</a>
-            <a class="navbar" href='index.php?page=user'>Utilisateurs</a>
-            <a class="navbar" href='index.php?page=settings'>Paramètres</a>
-            <a class="navbar" href='index.php?page=connexion'>Connexion</a>
+           <a href='index.php?page=accueil'>Accueil</a>
+           <a href='index.php?page=user'>Utilisateurs</a>
+           <a href='index.php?page=settings'>Paramètres</a>
+           
+           <?php 
+                if (isset($_SESSION) and !empty($_SESSION)){ ?>
+                     <a href="?page=connexion">Déconnexion</a>
+                <?php } else { ?>
+                    <a href="?page=connexion">Connexion</a>
+                <?php }
+                ?>
 
 
         </ul>
@@ -40,50 +48,152 @@ session_start()
     </nav>
 
 
-    <form class=form method="POST">
-
-        <p>Identifiant</p><br>
-        <input type="text" name='identifiant'> . <br>
-        <br>
-        <p>Mot de passe</p><br>
-        <input type="password" name='password'> . <br>
-        <br>
-        <input type="submit" name='submit'>
-
-    </form>
-
     <?php
 
-        if (isset($_POST['submit'])) {
-            $_SESSION = [
-                'identifiant' => 'sekoubambs',
-                'password' => 100385,
-                'age' => 18 , 
-                'nom' => 'haidara',
-                'prenom' => 'amadou',
-                'role' => 'stagiaire',
-            ] ;
+        if (isset($_GET['page']) && $_GET['page'] == "accueil" && empty($_SESSION)) {
+
+            echo '<form class="form" method="POST">';
+            echo '<p>Identifiant</p><input type="text" name="identifiant">';
+            echo '<p>Mot de passe</p><input type="password" name="password">';
+            echo '<br>';
+            echo '<br>';
+            echo '<input type="submit" name="submit" value="connexion">';
+            echo '</form>';
+    
         }
-var_dump($_SESSION);
-
-        $identifiant = $_POST['identifiant'];
-        $password = $_POST['password'];
+    ?>
 
 
-        if (isset($_POST['submit']) && !empty($_POST['identifiant'] == $_SESSION['identifiant']) && !empty($_POST['password'] == $_SESSION ['password'])){
+    <?php
+ 
 
-            echo "Vous êtes connectés";
-           
-        }
 
+// *********************************************** Accueil ********************************************************************************************
+ 
+if (isset($_POST['submit']) && ($_POST['identifiant'] == 'sekoubambs' && $_POST['password'] === '100385')) {
+
+    $_SESSION = ['nom' => 'haidara', 'prenom' => 'amadou', 'role' => 'stagiaire', 'age' => 38] ; 
+
+    $_SESSION = [
+        'identifiant' => 'sekoubambs',
+        'password' => '100385',
+        'age' => 38 , 
+        'nom' => 'haidara',
+        'prenom' => 'amadou',
+        'role' => 'stagiaire',
+    ] ;
+    
+          echo 'Bienvenue ' . $_SESSION['prenom'] . '!!!';
+
+        
+      }
+
+  if (isset($_POST['submit']) && ($_POST['identifiant'] != 'sekoubambs' || $_POST['password'] != '100385')) {
       
+      echo 'Votre identifiant ou votre mot de passe est incorrect' ;
+  }
 
+  if (isset($_GET['page']) && $_GET['page'] == "accueil" && !empty($_SESSION)) {
+      ?>
+      <h1>Bonjour et bienvenue sur votre page d'accueil !!!</h1>
+      <p class="alert-success">Vous êtes maintenant Connecté</p>
+      <?php
+      }
+
+
+// *********************************************** User ********************************************************************************************
+
+
+        if (isset($_GET['page']) && $_GET['page'] == 'user' && empty($_SESSION)){ ?>
+                <p class="alert warning">Vous devez être connecté pour pouvoir avoir accès à cette partie du site</p>
+            <?php }
+        
+        if (isset($_GET['page']) && $_GET['page'] == "user" && !empty($_SESSION) ) {
+
+            
+            echo '<h1>Vos informations utilisateur</h1>' . '<br>' ;
+            echo '<strong>Nom:</strong> ' . $_SESSION['nom'] . '<br>';
+            echo '<strong>Prénom:</strong> ' . $_SESSION['prenom'] . '<br>' ;
+            echo '<strong>Age:</strong> ' . $_SESSION['age'] . '<br>' ;
+            echo '<strong>Rôle:</strong> ' . $_SESSION['role'] . '<br>' ;
+            
+        }
+
+
+// *********************************************** Settings ********************************************************************************************
+           
+        if (isset($_GET['page']) && $_GET['page'] == "settings" && empty($_SESSION)) { ?>
+        <p class="alert warning">Vous devez être connecté pour pouvoir avoir accès à cette partie du site</p>
+        <?php }
+        
+        if (isset($_GET['page']) && $_GET['page'] == 'settings' && !empty($_SESSION)){ ?>
+
+            <form action="index.php"  method="POST" class="formConnexion" >
+                <h1>Modification de vos paramètres</h1>
+                <label for="nom">Nom</label>
+                <input type="text" name="nom" value="<?php echo $_SESSION['nom']; ?>">
+                <br>
+                <label for="prenom">Prénom</label>
+                <input type="text" name="prenom" value="<?php echo $_SESSION['prenom']; ?>">
+                <br>
+                <label for="age">Age</label>
+                <input type="number" name="age" value="<?php echo $_SESSION['age']; ?>">
+                <br>
+                <label for="role">Rôle</label>
+                <input type="text" name="role" value="<?php echo $_SESSION['role']; ?>">
+                <br>
+                <br>
+                <input type="submit" name="submitUpdate" value="Modifier">
+            </form>
+
+        <?php }
+
+
+        if (isset($_POST['submitUpdate'])){
+            if (empty($_POST['prenom']) || empty($_POST['nom']) || empty($_POST['age']) || empty($_POST['role'])){ ?>
+                <p class="alert-error">Toutes les informations ont besoin d'être renseigné</p>
+
+        <?php }
+
+            else{
+                    $_SESSION['prenom'] = $_POST['prenom'];
+                    $_SESSION['nom'] = $_POST['nom'];
+                    $_SESSION['age'] = $_POST['age'];
+                    $_SESSION['role'] = $_POST['role'];
+        ?>
+        <p class="alert-success">Les données utilisateurs ont bien été mis à jour</p>
+
+        <?php }
+}
+ 
+
+// *********************************************** Déconnexion ********************************************************************************************
+
+
+        if (isset($_GET['page']) && $_GET['page'] == "connexion" && !empty($_SESSION)) {
+
+            echo '<form method="POST">';
+            echo '<br>';
+            echo '<input type="submit" name="deconnexion" value="Déconnexion">';
+            echo '</form>';
+        }
 
     ?>
+        
+        <?php
+            if (isset($_POST['deconnexion'])) {
+            session_destroy(); ?>
+            <p class="alert-deconnexion">Vous êtes maintenant déconnecté</p>
+            <?php
+             }
+        ?>
     
 
 
 </section>
+
+</div>
+
 
 </body>
 </html>
